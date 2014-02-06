32de3f4d44427c19d178c8c70bc1741cabdcbe27
nick <nick@bodydatasystems.com>
2014-02-05 22:05:22 -0800

    new config location

--- js/test.js
+++ js/test.js
@@ -1,8 +1,7 @@
 var TEST = (function(module) {
   module.PushMe = function(resultObject) {
     resultObject.prepend(
-      Date() + '<br/>' +
-      device.uuid + '</br>'
+      Date() + '<br/>'
     );
   };
 
